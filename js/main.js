655ee1e8a09edfc315420537e6d1e099d2da6682
KeeKRaZy <78360343+KeeKRaZy@users.noreply.github.com>
2023-02-11 15:38:09 +0200

    Add files via upload
    Istructions added

--- js/main.js
+++ js/main.js
@@ -23,38 +23,3 @@ window.onload = () => {
     }
 }
 
-/* button */
-
-const hamb = document.querySelector(".hamb");
-const popup = document.querySelector("#popup");
-const body = document.body;
-
-
-// При клике на иконку hamb вызываем ф-ию hambHandler
-hamb.addEventListener("click", hambHandler);
-
-// Выполняем действия при клике ..
-function hambHandler(e) {
-  e.preventDefault();
-  // Переключаем стили элементов при клике
-  popup.classList.toggle("open");
-  renderPopup();
-}
-
-// Здесь мы рендерим элементы в наш попап
-function renderPopup() {
-  popup.appendChild(menu);
-}
-
-// Код для закрытия меню при нажатии на ссылку
-const links = Array.from(menu.children);
-
-// Для каждого элемента меню при клике вызываем ф-ию
-links.forEach((link) => {
-  link.addEventListener("click", closeOnClick);
-});
-
-// Закрытие попапа при клике на меню
-function closeOnClick() {
-  popup.classList.remove("open");
-}
